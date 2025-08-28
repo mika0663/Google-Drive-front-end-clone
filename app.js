4e9f6eaff5a097bd1df3b59e7423061aa7e9ba2a
Muhammad Ikram <mikrams0663@gmail.com>
2025-08-28 22:25:40 +0500

    Complete backend- nuturing frontend

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const userRouter = require("./routes/user.routes");
-
+const dotenv = require("dotenv")
+dotenv.config();
+const connectToDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+const indexRouter = require('./routes/index.routes')
+connectToDB();
 
 
 app.set('view engine', 'ejs');
 app.use(express.json()); 
+app.use(cookieParser())
 app.use(express.urlencoded({extended: true })); // to parse form data
 
 app.use('/user', userRouter);
+app.use('/', indexRouter);
 
 app.listen(3000, ()=>{
     console.log("Server running on port 3000");
